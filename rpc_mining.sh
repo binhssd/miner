0fabd5663e837b2c346ab106c291721843519816
binhssd <139931697+binhssd@users.noreply.github.com>
2023-09-16 17:43:45 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qyjsm45e7qj23rhqdawe3v2j8ujcfxygquw330dn4x0jfduds5mtwqgqkdmjl -r community-pools.mysrv.cloud:10100 -m 7 -p rpc;
+    sleep 5;
+done
